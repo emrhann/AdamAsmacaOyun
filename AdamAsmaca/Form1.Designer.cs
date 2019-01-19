59d5a41f2684777a25b201a7239f6c5ff03c7c6c
Emirhan IŞIK <emir94han@hotmail.com>
2019-01-19 16:01:29 +0300

    AdamAsmaca

---
+++ AdamAsmaca/Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace AdamAsmaca
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button_Sil = new System.Windows.Forms.Button();
+            this.button_Ekle = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.tahmin_buton = new System.Windows.Forms.Button();
+            this.tahmin_textbox = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(205, 229);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hak Sayısı:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(316, 229);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "0";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button_Sil);
+            this.groupBox1.Controls.Add(this.button_Ekle);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox1.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(188, 133);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Dosya İşlemi";
+            // 
+            // button_Sil
+            // 
+            this.button_Sil.Location = new System.Drawing.Point(6, 95);
+            this.button_Sil.Name = "button_Sil";
+            this.button_Sil.Size = new System.Drawing.Size(177, 32);
+            this.button_Sil.TabIndex = 1;
+            this.button_Sil.Text = "SİL";
+            this.button_Sil.UseVisualStyleBackColor = true;
+            this.button_Sil.Click += new System.EventHandler(this.button_Sil_Click);
+            // 
+            // button_Ekle
+            // 
+            this.button_Ekle.Location = new System.Drawing.Point(6, 57);
+            this.button_Ekle.Name = "button_Ekle";
+            this.button_Ekle.Size = new System.Drawing.Size(177, 32);
+            this.button_Ekle.TabIndex = 1;
+            this.button_Ekle.Text = "EKLE";
+            this.button_Ekle.UseVisualStyleBackColor = true;
+            this.button_Ekle.Click += new System.EventHandler(this.button_Ekle_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(177, 26);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(206, 14);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(231, 32);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Rastgele Kelime";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.numericUpDown1.Location = new System.Drawing.Point(443, 52);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(231, 22);
+            this.numericUpDown1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(443, 14);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(231, 32);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Belirli Sayıda";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(571, 226);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(103, 22);
+            this.textBox3.TabIndex = 6;
+            // 
+            // tahmin_buton
+            // 
+            this.tahmin_buton.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tahmin_buton.Location = new System.Drawing.Point(571, 124);
+            this.tahmin_buton.Name = "tahmin_buton";
+            this.tahmin_buton.Size = new System.Drawing.Size(103, 96);
+            this.tahmin_buton.TabIndex = 7;
+            this.tahmin_buton.Text = "TAHMİN";
+            this.tahmin_buton.UseVisualStyleBackColor = true;
+            this.tahmin_buton.Click += new System.EventHandler(this.tahmin_buton_Click);
+            // 
+            // tahmin_textbox
+            // 
+            this.tahmin_textbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tahmin_textbox.Location = new System.Drawing.Point(571, 88);
+            this.tahmin_textbox.MaxLength = 1;
+            this.tahmin_textbox.Multiline = true;
+            this.tahmin_textbox.Name = "tahmin_textbox";
+            this.tahmin_textbox.Size = new System.Drawing.Size(103, 30);
+            this.tahmin_textbox.TabIndex = 8;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(18, 151);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(177, 100);
+            this.listBox1.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(345, 230);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 18);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Yanlış Harfler:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(465, 224);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 17);
+            this.label4.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(207, 54);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(89, 17);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Oyuncu Adı: ";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(302, 52);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(135, 22);
+            this.textBox2.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(681, 255);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.tahmin_textbox);
+            this.Controls.Add(this.tahmin_buton);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "ADAM ASMACA --- EMİRHAN IŞIK";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button_Sil;
+        private System.Windows.Forms.Button button_Ekle;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button tahmin_buton;
+        private System.Windows.Forms.TextBox tahmin_textbox;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ AdamAsmaca/Form1.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace AdamAsmaca
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string kelime_dosya_yolu = Application.StartupPath + "/kelimeler.txt";
+        string skor_dosya_yolu = Application.StartupPath + "/skorlist.txt";
+        string üretilen_kelime;
+        int hak_sayi=0;
+        Label[] lbl = new Label[12];
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tahmin_buton.Enabled = false;
+            tahmin_textbox.Enabled = false;
+
+            string[] dizi1 = File.ReadAllLines(skor_dosya_yolu);
+            for (int i = 0; i < dizi1.Length; i++)
+            {
+                listBox1.Items.Add(dizi1[i]);
+            }
+
+        }
+        public void DosyayaYaz(string kelime)
+        {
+            if (File.Exists(kelime_dosya_yolu))
+            {
+                //Dosya varsa
+                StreamWriter yaz = new StreamWriter(kelime_dosya_yolu, true);
+                yaz.WriteLine(kelime);
+                yaz.Close();
+            }
+        }
+        public void Skor_Ekle()
+        {
+            if (File.Exists(skor_dosya_yolu))
+            {
+                //Dosya varsa
+                StreamWriter yaz = new StreamWriter(skor_dosya_yolu, true);
+                yaz.WriteLine(textBox2.Text +":"+hak_sayi.ToString());
+                yaz.Close();
+            }
+
+        }
+        public void Tahmin(string harf)
+        {
+            bool kntrl=false;
+            bool kntrl2 = false;
+            
+            char[] dizi_harf = üretilen_kelime.ToCharArray();
+            for (int i = 0; i < dizi_harf.Length; i++)
+            {
+                if (harf==dizi_harf[i].ToString())
+                {
+                    lbl[i].Text = dizi_harf[i].ToString();
+                    kntrl = true;
+                    
+                }
+            }
+            // Bilinemeyen harfleri labelden alıp char dizisi oluştuşturup o şekilde kontrol ettim.
+            char[] dizi_harf2 = label4.Text.ToCharArray();
+            for (int i = 0; i < dizi_harf2.Length; i++)
+             {
+                if (harf==dizi_harf2[i].ToString())
+                {
+                    MessageBox.Show("DİKKAT Bilinemeyen aynı harfi girdiniz !");
+                    kntrl2 = true;
+                }
+              }
+            if (kntrl==false) // Bulamazsa harfi dışarı yazdırıyor
+            {
+                if(kntrl2==false)
+                {
+                    label4.Text = label4.Text + harf;
+                    hak_sayi = hak_sayi - 1;
+                }
+                
+            }
+            label2.Text = hak_sayi.ToString();
+            int x = 0;
+            for (int i = 0; i < dizi_harf.Length; i++)
+            {
+                if (lbl[i].Text == dizi_harf[i].ToString())
+                {
+                    x++;
+                }
+            }
+            if (x==dizi_harf.Length)
+            {
+                MessageBox.Show("!.. TEBRİKLER KAZANDINIZ ..!");
+                Skor_Ekle();
+            }
+
+            if(hak_sayi==0)
+            {
+                MessageBox.Show("GAME OVER CINIM"+ "  Kelime : "+üretilen_kelime);
+                tahmin_buton.Enabled = false;
+                label4.Text = null;
+                button1.Enabled = true;
+                button3.Enabled = true;
+                üretilen_kelime = null;
+                Array.Clear(lbl, 0, lbl.Length);
+                Skor_Ekle();
+            }
+        }
+
+        public void Belirli_sayida(int sayi)
+        {
+            bool deger=false;
+            if (sayi<3)
+            {
+                MessageBox.Show("En az 3 harfli sayi üretilebilir");
+            }
+            else
+            {
+             string[] K_dizi = File.ReadAllLines(kelime_dosya_yolu);
+                for (int i = 0; i < K_dizi.Length; i++)
+                {
+                char[] dizi_harf = K_dizi[i].ToCharArray();
+                    if (dizi_harf.Length==sayi)
+                    {
+                        deger = true;
+                        üretilen_kelime=K_dizi[i];
+                        for (int j = 0; j < dizi_harf.Length; j++)
+                        {
+                            lbl[j] = new Label();
+                            lbl[j].AutoSize = true;
+                            lbl[j].Location = new Point(200 + (20 * j), 100);
+                            lbl[j].Text = "----";
+                            Controls.Add(lbl[j]);
+                        }
+                       break;
+                    }
+                }
+                if (deger==false)
+                    {
+                        MessageBox.Show("Belirtilen sayıda harften oluşan kelime yok !");
+                        üretilen_kelime = null;
+                    }
+            }
+            button3.Enabled = false;
+            button1.Enabled = false;
+        }
+    
+        public void Rastgele_üret()
+        {
+            Random rnd = new Random();
+
+            string[] K_dizi = File.ReadAllLines(kelime_dosya_yolu);
+            int randm = rnd.Next(0, K_dizi.Length);
+            char[] dizi_harf = K_dizi[randm].ToCharArray();
+            üretilen_kelime = K_dizi[randm];
+            for (int i = 0; i < dizi_harf.Length; i++)
+            {
+                lbl[i] = new Label();
+                lbl[i].AutoSize = true;
+                lbl[i].Location = new Point(200 + (20 * i), 100);
+                lbl[i].Text = "----";
+                Controls.Add(lbl[i]);
+            }
+            button3.Enabled = false;
+            button1.Enabled = false;
+        }
+
+        public void DosyadanSil(string silinecek_kelime)
+        {
+             string[] dizi1 = File.ReadAllLines(kelime_dosya_yolu);
+            if (File.Exists(kelime_dosya_yolu))
+            {
+                //Dosya varsa
+                StreamWriter yaz = new StreamWriter(kelime_dosya_yolu);
+                yaz.Write("");
+                yaz.Close();
+            }
+            List<string> liste = new List<string>(dizi1);
+
+            liste.Remove(silinecek_kelime);
+            if (File.Exists(kelime_dosya_yolu))
+            {
+                StreamWriter yaz = new StreamWriter(kelime_dosya_yolu,true);
+                for (int i = 0; i < liste.Count; i++)
+                {
+                    yaz.WriteLine(liste[i]);   
+                }
+                yaz.Close();
+            }
+        }
+
+        private void button_Ekle_Click(object sender, EventArgs e)
+        {
+            DosyayaYaz(textBox1.Text);
+        }
+
+        private void button_Sil_Click(object sender, EventArgs e)
+        {
+            DosyadanSil(textBox1.Text.ToUpper());
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if(textBox2.Text=="")
+            {
+                MessageBox.Show("Lütfen Önce Adınızı Giriniz");
+            }
+            else
+            {
+            Rastgele_üret();
+            tahmin_buton.Enabled = true;
+            tahmin_textbox.Enabled = true;
+            textBox3.Text = üretilen_kelime;
+            hak_sayi = üretilen_kelime.Length + 2;
+            label2.Text = hak_sayi.ToString();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "")
+            {
+                MessageBox.Show("Lütfen Önce Adınızı Giriniz");
+            }
+            else
+            {
+                tahmin_buton.Enabled = true;
+                tahmin_textbox.Enabled = true;
+                Belirli_sayida(Convert.ToInt16(numericUpDown1.Value));
+                if(üretilen_kelime==null)
+                {
+                    hak_sayi = 0;
+                }
+                else
+                {
+                    textBox3.Text =üretilen_kelime;
+                    hak_sayi = üretilen_kelime.Length + 2;
+                }
+            
+                label2.Text = hak_sayi.ToString();
+            }
+            
+        }
+
+        private void tahmin_buton_Click(object sender, EventArgs e)
+        {
+            Tahmin(tahmin_textbox.Text.ToUpper());
+        }
+    }
+}
